76c94d6091a1fa8e7171c43f05e16ce1092f0f66
sberzonskiene <s.berzonskiene@gmail.com>
2025-07-23 13:37:01 +0300

    update

--- script.js
+++ script.js
@@ -2,16 +2,27 @@ const kingBooksEl = document.getElementById("king-books");
 const loaderEl = document.getElementById("loader");
 const tableEl = document.getElementById("table");
 
+const modalWindow = document.createElement("div");
+modalWindow.id = "modal";
+
+const closeBtn = document.createElement("div");
+closeBtn.classList.add("close-modal");
+closeBtn.textContent = "x";
+
+const contentEl = document.createElement("div");
+contentEl.classList.add("api-content");
+
+modalWindow.appendChild(closeBtn);
+modalWindow.appendChild(contentEl);
+
 tableEl.classList.add('hide');
 loaderEl.classList.add('show');
 
 fetch('https://stephen-king-api.onrender.com/api/books')
 .then((resp) => resp.json())
 .then((data) => {
-
-    tableEl.classList.remove('hide')
     loaderEl.classList.remove('show')
-
+    tableEl.classList.remove('hide')
     data.data.forEach((book) => {
         kingBooksEl.insertAdjacentHTML('beforeend', 
             `<tr data=bookid="${book.id}">
@@ -35,20 +46,46 @@ kingBooksEl.addEventListener('click', (e) => {
         kingBooksEl.removeChild(villainsRowEl);
     }
 
-    if (villainsRowEl !== tr) {
+    if (villainsRowEl != tr) {
     const bid = tr.dataset.bookid;
-    fetch('https://stephen-king-api.onrender.com/api/book/' + bid)
-    .then(res => res.json())
-    .then((data) => {
+    fetch("https://stephen-king-api.onrender.com/api/book/" + bid)
+      .then((res) => res.json())
+      .then((data) => {
         tr.insertAdjacentHTML(
-            'afterend',
-            `<tr id="villains-row">
-                <td colspan="2">${data.data.Title}</td>
-                <td colspan="4">${data.data.villains.map((villain) => villain.name).join("<br/>")}</td>
-            </tr>`
+          "afterend",
+          `<tr id="villains-row">
+        <td colspan="2">${data.data.Title}</td>
+        <td colspan="4">${data.data.villains
+          .map((villain) => `<a href="${villain.url}">${villain.name}</a>`)
+          .join("<br/>")}</td>
+          </tr>`
         );
-    }) 
-    .catch((err) => console.log(err));
-    }
+        const links = Array.from(document.getElementsByTagName("a"));
+        links &&
+          links.forEach((link) => {
+            // console.log(link.href);
+            link.addEventListener("click", (e) => {
+              e.stopPropagation();
+              e.preventDefault();
+              console.log(e.target);
+
+              fetch(e.target.href)
+              .then((res) => res.json())
+              .then((data) => { console.log(data);
+                contentEl.innerHTML = `
+                <h2>${data.data.name}</h2>
+                <p>${data.data.status}</p>
+                <ul>${data.data.books.map((book) => `<li>${book.title}</li>`)
+                .join("")}}</ul>
+                `;
+                console.log(contentEl);
+                
+                document.body.appendChild(modalWindow);
+              });
+            });
+          });
+      })
+      .catch((err) => console.log(err));
+  }
 });
 
